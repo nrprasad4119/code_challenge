2cf57dbf9de1f171758847eb389b7010ec283ebe
nrprasad4119 <nrprasad4119@gmail.com>
2023-09-25 21:36:14 +0530

    add files

---
+++ Challenge #2/Azure_metadata.ps1
@@ -0,0 +1,17 @@
+﻿# Define the URL for Azure instance metadata
+$metadataUrl = "http://169.254.169.254/metadata/instance?api-version=2021-09-01"
+
+# Set headers for the request
+$headers = @{
+    "Metadata" = "true"
+}
+
+# Make the HTTP request to retrieve instance metadata
+try {
+    $instanceMetadata = Invoke-RestMethod -Uri $metadataUrl -Headers $headers -Method Get -TimeoutSec 2
+    $instanceMetadata | ConvertTo-Json | Out-File -FilePath instance_metadata.json -Encoding UTF8
+    Write-Host "Instance metadata saved to instance_metadata.json"
+}
+catch {
+    Write-Host "Error retrieving instance metadata: $_"
+}
---
+++ Challenge #3/nested-object-function.ps1
@@ -0,0 +1,45 @@
+﻿function GetValueFromNestedObject {
+    param (
+        [Parameter(Mandatory=$true)]
+        [System.Object]$inputObject,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$key
+    )
+
+    $keys = $key -split '\.'
+    $currentObject = $inputObject
+
+    foreach ($k in $keys) {
+        if ($currentObject -is [System.Collections.IDictionary]) {
+            $currentObject = $currentObject[$k]
+        }
+        else {
+            $currentObject = $currentObject.$k
+        }
+
+        if ($currentObject -eq $null) {
+            Write-Host "Key '$key' not found in the nested object." -ForegroundColor Red
+            return $null
+        }
+    }
+
+    return $currentObject
+}
+
+# Example Usage:
+$nestedObject = @{
+    'firstLevel' = @{
+        'secondLevel' = @{
+            'thirdLevel' = 'Hello World'
+        }
+    }
+}
+
+$key = 'firstLevel.secondLevel.thirdLevel'
+
+$value = GetValueFromNestedObject -inputObject $nestedObject -key $key
+
+if ($value -ne $null) {
+    Write-Host "Value for key '$key' is '$value'" -ForegroundColor Green
+}
